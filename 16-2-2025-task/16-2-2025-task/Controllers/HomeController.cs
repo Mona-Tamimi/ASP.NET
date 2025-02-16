504bb52c7d2f1d3c51ec04a499169ca0903dfb5a
Mona-Tamimi <munatamimi65@gmail.com>
2025-02-16 20:54:20 +0300

    ASP .NET Task

---
+++ 16-2-2025-task/16-2-2025-task/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using _16_2_2025_task.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _16_2_2025_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult register()
+        {
+            return View();
+        }
+        public IActionResult services()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
+        public IActionResult consultant()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
