736ba0257eb9dc2142fe7a87bc3d911331a30ac2
Mona-Tamimi <munatamimi65@gmail.com>
2025-02-17 17:05:12 +0300

    ASP .NET Task

---
+++ 17-2-2025-task/17-2-2025-task/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _17_2_2025_task.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
